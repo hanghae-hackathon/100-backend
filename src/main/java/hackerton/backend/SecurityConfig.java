c8190e5cbffabceb41e58ea72db920e0b468f310
dami0806 <dami0804@naver.com>
2024-06-01 05:39:44 +0900

    feat: get swagger

---
+++ src/main/java/hackerton/backend/SecurityConfig.java
@@ -0,0 +1,26 @@
+package hackerton.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
--- src/main/java/hackerton/backend/dog/service/DogService.java
+++ src/main/java/hackerton/backend/dog/service/DogService.java
@@ -1,14 +1,11 @@
 package hackerton.backend.dog.service;
 
-import hackerton.backend.dog.dto.DogRequestDto;
-import hackerton.backend.dog.dto.DogResponseDto;
 import hackerton.backend.dog.entity.DogEntity;
 import hackerton.backend.dog.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class DogService {
